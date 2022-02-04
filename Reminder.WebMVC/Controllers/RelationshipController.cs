efc043bde0a15fc9b4c97eff9a18c91644d4fe98
Colin Dunn <dunncolinj@gmail.com>
2022-02-03 20:25:38 -0500

    20220203 20:25

---
+++ Reminder.WebMVC/Controllers/RelationshipController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reminder.WebMVC.Controllers
+{
+    public class RelationshipController : Controller
+    {
+        // GET: Relationship
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
